20f6a628c957414f5600699f0c960a354485cb8a
Kyojin Syo <kyojinsyo@gmail.com>
2016-09-11 13:50:45 +0900

    Initial Commit

---
+++ mbUsingTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mbUsingTest
+//
+//  Created by 蕭　喬仁 on 2016/09/11.
+//  Copyright © 2016年 蕭　喬仁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
